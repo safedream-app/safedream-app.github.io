743fa0f0154c1571e2670c33a4353d478e8763f2
kokohuhusave <92168240+kokohuhusave@users.noreply.github.com>
2023-05-29 15:56:13 +0900

    Update signup.js
    중복확인,회원가입 수정

--- signup.js
+++ signup.js
@@ -22,6 +22,12 @@ function registerUser() {
     const hp = document.getElementById('hp').value;
     const guardianHp = document.getElementById('guardianHp').value;
 
+    // 추가된 부분: 입력값 확인
+    if (!username || !userid || !password || !userpw_a || !hp || !guardianHp) {
+        alert('모든 개인정보를 채워주세요.');
+        return;
+    }
+
     if (password !== userpw_a) {
         alert('비밀번호가 일치하지 않습니다. 다시 확인해주세요.');
         return;
@@ -40,7 +46,7 @@ function registerUser() {
     .then(data => {
         if (data.message) {
             alert(data.message);
-            location.href='./index.html';
+            location.href='./login_edit.html';
         }
     });
 }
